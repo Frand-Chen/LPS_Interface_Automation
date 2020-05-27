c1d65660cb9bc63824babe88a5b5c25aa9315dd8
frand_chen <frand_chen@163.com>
2020-05-28 00:36:18 +0800

    更新handlele_data

--- common/handle_data.py
+++ common/handle_data.py
@@ -5,10 +5,39 @@
 """
 
 import re
+from common.handle_config import conf
+
 
 class TestData:
     """保存需要替换的数据"""
     pass
 
-def replace_data():
-    pass
+
+def replace_data(data):
+    # 匹配规则
+    r = r"#(.+?)#"
+    # 根据规则，判断是否需要替换数据
+    while re.search(r, data):
+        # 匹配第一个要替换的数据
+        res = re.search(r, data)
+        # 提取待替换的数据 #***#
+        item = res.group()
+        # 获取替换内容中的数据项
+        key = res.group(1)
+
+        try:
+            # 根据替换内容 item"#***#" 到配置文件中查找对应的数据 key "***"，并进行替换
+            data = data.replace(item, str(conf.get("test_data", key)))
+        except:
+            # 如果配置文件中没有对应的 key,则到 TestDate 中查找,
+            data = data.replace(item, str(getattr(TestData(), key)))
+
+    return data
+
+
+if __name__ == '__main__':
+    # data = replace_data("#phone#,#name#")
+    dic = '{"member_id":#admin_member_id#+1}'
+    data = replace_data(dic)
+    print(type(data))
+    print(eval(data))
