57248dc79be63bc2da09499efd1bf9241e1b9c7a
frand_chen <frand_chen@163.com>
2020-05-31 14:57:37 +0800

    jenkins 部署

--- run_test.py
+++ run_test.py
@@ -1,5 +1,16 @@
 # -*- coding=utf-8 -*-
 
-import pytest
+import os
 
-pytest.main()
+import pytest
+import time
+ts = str(int(time.time()))
+
+# 报告一，生成 allure 报告
+# 执行pytest单元测试，生成 Allure 报告需要的数据存在 result 目录
+pytest.main(['-s',"--alluredir=report_data"])
+# 执行命令 allure generate ./result -o ./report --clean ，生成测试报告
+os.system('allure generate ./report_data -o ./report --clean')
+
+# 报告二，生成 html 报告
+# pytest.main(['-m test', '-s',"--html=reports/report_{}.html".format(ts)])
