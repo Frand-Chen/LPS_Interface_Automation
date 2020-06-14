e30a27d21ce4d975e94346d8ba8f77812daa5f24
frand_chen <frand_chen@163.com>
2020-06-14 22:58:46 +0800

    新增testst_coupons 测试用例模块

---
+++ testcases/test_coupons.py
@@ -0,0 +1,13 @@
+# -*- coding=utf-8 -*-
+
+"""
+coupons 测试用例模块
+"""
+
+import allure
+
+@allure.feature("获取 Coupon 信息接口")
+class TestCoupons:
+    """执行 Coupons 测试用例"""
+    # 获取 Coupons 的测试用例数据
+    pass
