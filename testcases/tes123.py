f9a47616fc043d1edf68e6ec7042ec0e6cb2db61
frand_chen <frand_chen@163.com>
2021-06-29 22:15:35 +0800

    review

--- testcases/tes123.py
+++ testcases/tes123.py
@@ -1,25 +1,26 @@
 # -*- coding=utf-8 -*-
-import json
-import pytest
-from jsonpath import jsonpath
-from openpyxl.styles import colors
-from common.read_excel import ReadExcel
-from testcases import *
+# import json
+# import pytest
+# from jsonpath import jsonpath
+# from openpyxl.styles import colors
+# from common.read_excel import ReadExcel
+# from testcases import *
+#
+# title = """{
+#     "matchCount": 0,
+#     "nextResultSetExists": false,
+#     "previousResultSetExists": false,
+#     "pageIndex": 0,
+#     "pageSize": 1,
+#     "members": []
+# }"""
+#
+# title_json = json.loads(title)
+# print(jsonpath(title_json, "$.matchCount")[0])
+# print(jsonpath(title_json, "$.members"))[0]
+# print(type(title_json))
 
-title = """{
-    "matchCount": 0,
-    "nextResultSetExists": false,
-    "previousResultSetExists": false,
-    "pageIndex": 0,
-    "pageSize": 1,
-    "members": []
-}"""
+# title_str = json.dumps(title_json)
 
-title_json = json.loads(title)
-print(jsonpath(title_json, "$.matchCount")[0])
-print(jsonpath(title_json, "$.members"))[0]
-print(type(title_json))
-
-title_str = json.dumps(title_json)
-
-print(type(title_str))
+# print(type(title_str))
+print("ddddddd")
